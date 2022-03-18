b0ccef6052e1fb27c401fbaaa5d0cb41efefacb1
Thages Almeida <thageswilson@gmail.com>
2022-03-18 18:40:26 -0300

    Bisection

---
+++ BracketingMethods.Tests/BracketingMethodsService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bisection.Services;
+
+namespace Bisection.UnitTests.Services
+{
+    [TestClass]
+    public class BisectionService_Implement
+    {
+        private readonly BisectionServices _bisectionService;
+
+        public BisectionService_Implement()
+        {
+            _bisectionService = new BisectionServices();
+        }
+
+        [TestMethod]
+        public void TestBisect_ReturnValue()
+        {
+            var result = _bisectionService.Bisect(12,16,14,0.5,5);
+
+            Assert.AreEqual(14.8125, result, $"{result} should be 14.8125");
+        }
+
+        #region Sample_TestCode
+        [DataTestMethod]
+        [DataRow(12, 16, 14, 0.5, 2)]
+        [DataRow(12, 16, 14, 0.5, 3)]
+        [DataRow(12, 16, 14, 0.5, 4)]
+        public void TestBisect_ReturnFalse(double xl, double xu, double xr, double es, int imax)
+        {
+            var result = _bisectionService.Bisect(xl, xu, xr, es, imax);
+
+            Assert.AreNotEqual(xr, $"xr value should be {xr}");
+        }
+        #endregion
+    }
+}
---
+++ BracketingMethods/Bisection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bisection.Services
+{
+    public class BisectionServices
+    {
+        private double xrold;
+        private double test;
+        private double ea;
+        private int i;
+
+        public double Bisect(double xl, double xu, double xr, double es, int imax)
+        {
+            Console.WriteLine($" Iter |   Xl   |   Xu   |   Xr   |   Ea\n");
+            while (ea > es || i <= imax)
+            {
+                xrold = xr;
+                xr = R((xl + xu) / 2);
+
+                if (xr != 0)
+                {
+                    ea = R(Math.Abs((xr - xrold) / xr) * 100);
+                }
+                test = R(F(xl) * F(xr));
+
+                if (test < 0)
+                {
+                    xu = xr;
+                } else if (test > 0) {
+                    xl = xr;
+                } else
+                {
+                    ea = 0;
+                }
+                Console.WriteLine($"  {i}   |   {xl}   |   {xu}   |   {xr}   |   {ea}\n");
+                i++;
+            }
+
+            return xr;
+
+            throw new NotImplementedException("Please create a test first");
+        }
+
+        private double F(double x)
+        {
+            return Math.Sin(10 * x) + Math.Cos(3 * x);
+        }
+
+       
+        private double R(double value)
+        {
+            double power = Math.Pow(10.0, 4);
+            return Math.Truncate((power * value)) / power;
+        }
+    }
+}
